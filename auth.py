f1be127c0102ed045421c3ff22b94c05236485e3
KirthivasanPN-hash <kirthivasanp@gmail.com>
2025-05-31 19:54:40 -0400

    env changes

--- auth.py
+++ auth.py
@@ -3,9 +3,7 @@
 import streamlit as st
 import requests
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 API_URL = os.getenv("API_URL", "https://newsletter-app-ce6e.onrender.com")
 
 def initialize_session_state():
